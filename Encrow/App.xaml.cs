e3589fe3095001294b575d3448ec35fcd57d96b6
bolfosu <bolfamihail@gmail.com>
2024-04-27 00:55:43 +0200

    Initial commit

---
+++ Encrow/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Encrow
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
