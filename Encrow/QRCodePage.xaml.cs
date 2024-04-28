e268a0968d7fc176f8bf5701bb29716e351b7056
bolfosu <bolfamihail@gmail.com>
2024-04-29 00:08:16 +0200

    main page buttons functionality

--- Encrow/QRCodePage.xaml.cs
+++ Encrow/QRCodePage.xaml.cs
@@ -20,7 +20,7 @@ namespace Encrow
         private void OnGenerateClicked(object sender, EventArgs e)
         {
             QRCodeGenerator qrGenerator = new QRCodeGenerator();
-            QRCodeData qrCodeData = qrGenerator.CreateQrCode(InputText.Text, QRCodeGenerator.ECCLevel.L);
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode("23", QRCodeGenerator.ECCLevel.L);
             PngByteQRCode qRCode = new PngByteQRCode(qrCodeData);
             byte[] qrCodeBytes = qRCode.GetGraphic(20);
             QrCodeImage.Source = ImageSource.FromStream(() => new MemoryStream(qrCodeBytes));
