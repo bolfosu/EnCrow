74e68f2c383be4f54a6cda50c634bebd55c782de
bolfosu <bolfamihail@gmail.com>
2024-05-01 22:21:07 +0200

    qrt scaning functionality

--- Encrow/VerifierPage.xaml.cs
+++ Encrow/VerifierPage.xaml.cs
@@ -1,9 +1,31 @@
+using ZXing.Mobile;
+
 namespace Encrow;
 
 public partial class VerifierPage : ContentPage
 {
-	public VerifierPage()
+    MobileBarcodeScanner scanner;
+
+    public VerifierPage()
 	{
 		InitializeComponent();
-	}
+
+		scanner =  new MobileBarcodeScanner();
+        {
+        
+        };
+    }
+    async void ScanButton_Clicked(object sender, EventArgs e)
+    {
+        var result = await scanner.Scan();
+
+        if (result != null)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            { DisplayAlert("Scanned QR Code", result.Text, "OK");
+            });
+        }
+    }
+
+
 }
