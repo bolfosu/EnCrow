ff9ffb3c42ed417fa4521dc462a2d02a014fe0b8
bolfosu <bolfamihail@gmail.com>
2024-05-02 20:08:13 +0200

    fixed cv errors

--- Encrow/VerifierPage.xaml.cs
+++ Encrow/VerifierPage.xaml.cs
@@ -1,31 +1,36 @@
+using Camera.MAUI;
 using ZXing.Mobile;
 
 namespace Encrow;
 
 public partial class VerifierPage : ContentPage
 {
-    MobileBarcodeScanner scanner;
-
+    
     public VerifierPage()
 	{
 		InitializeComponent();
 
-		scanner =  new MobileBarcodeScanner();
-        {
-        
-        };
     }
-    async void ScanButton_Clicked(object sender, EventArgs e)
+    private void cameraView_CamerasLoaded(object sender, EventArgs e)
     {
-        var result = await scanner.Scan();
-
-        if (result != null)
+        if (cameraView.Cameras.Count > 0)
         {
-            Device.BeginInvokeOnMainThread(() =>
-            { DisplayAlert("Scanned QR Code", result.Text, "OK");
+            cameraView.Camera = cameraView.Cameras.First();
+            MainThread.BeginInvokeOnMainThread(async () =>
+            {
+                await cameraView.StopCameraAsync();
+                await cameraView.StartCameraAsync();
             });
         }
     }
 
-
+    private void cameraView_BarcodeDetected(object sender, Camera.MAUI.ZXingHelper.BarcodeEventArgs args)
+    {
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            barcodeResult.Text = $"{args.Result[0].BarcodeFormat}: {args.Result[0].Text}";
+        });
+    }
 }
+
+
