94778c7811bebed519052262c6362e9c53b4aee2
bolfosu <bolfamihail@gmail.com>
2024-06-04 18:42:24 +0200

    test commit

--- Encrow/PinCodesPage.xaml.cs
+++ Encrow/PinCodesPage.xaml.cs
@@ -4,17 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+
 
 namespace Encrow
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
+   
     public partial class PinCodesPage : ContentPage
     {
         public PinCodesPage()
         {
-            InitializeComponent();
+            
         }
     }
 }
